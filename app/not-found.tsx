bb9b937c95b2aaf8c8e625e6f95a5e321def2c6b
Viktor <viktoryanenko11@gmail.com>
2025-07-04 12:35:31 +0300

    add meta for [id]/page and not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,31 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Page not found',
+  description: 'Page you are looking for does not exist',
+  openGraph: {
+    title: 'Page not found',
+    description: 'Page you are looking for does not exist',
+    url: `/not-found`,
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Page not found',
+      },
+    ],
+    type: 'article',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Page not found',
+    description: 'Page you are looking for does not exist',
+    images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+  },
+};
+
 export default function NotFound() {
   return (
     <div style={{ textAlign: 'center' }}>
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,57 @@
-import { fetchNoteById } from "../../../lib/api";
-import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import NoteDetailsClient from "./NoteDetails.client";
+import { fetchNoteById } from '../../../lib/api';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+import NoteDetailsClient from './NoteDetails.client';
 
 type Props = {
-    params: Promise<{ id: number }>;
+  params: Promise<{ id: number }>;
+};
+
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+  return {
+    title: `Note: ${note.title}`,
+    description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 100),
+      url: ``,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${note.title}`,
+      description: note.content.slice(0, 30),
+      images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    },
+  };
 }
 
 export default async function NoteDetails({ params }: Props) {
-    const res = await params;
-    const queryClient = new QueryClient();
+  const res = await params;
+  const queryClient = new QueryClient();
 
-    await queryClient.prefetchQuery({
-        queryKey: ['note', res.id],
-        queryFn: () => fetchNoteById(res.id),
-    })
+  await queryClient.prefetchQuery({
+    queryKey: ['note', res.id],
+    queryFn: () => fetchNoteById(res.id),
+  });
 
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <NoteDetailsClient />
-        </HydrationBoundary>
-    )
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
 }
